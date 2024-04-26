d722b063036340765817a5a4a7f968babbacb496
SarfDev <kingos1ei32@gmail.com>
2024-04-26 18:33:40 +0200

    okay

---
+++ components/Card.jsx
@@ -0,0 +1,25 @@
+export function Card({imgCampione, item1,item2,item3,item4,item5,item6,nome}){
+
+    return(
+
+        <div className='flex flex-col gap-4 items-start' style={{display:"flex", flexDirection:"column", gap:"16px",alignItems:"center"}}>
+
+            <img src={imgCampione} className='h-60' style={{height:"240px",objectFit:"cover"}}/>
+
+            <h2>{nome}</h2>
+
+            <div className='grid grid-cols-3 grid-rows-3 gap-2' style={{display:"grid",gridTemplateColumns:"repeat(3, minmax(0, 1fr))", gridTemplateRows:"repeat(3, minmax(0, 1fr))", gap:"8px"}}>
+
+                <img src={item1} className='h-16' style={{height:"64px",objectFit:"cover"}}/>
+                <img src={item2} className='h-16' style={{height:"64px",objectFit:"cover"}}/>
+                <img src={item3} className='h-16' style={{height:"64px",objectFit:"cover"}}/>
+
+                <img src={item4} className='h-16' style={{height:"64px",objectFit:"cover"}}/>
+                <img src={item5} className='h-16'style={{height:"64px",objectFit:"cover"}}/>
+                <img src={item6} className='h-16'style={{height:"64px",objectFit:"cover"}}/>
+
+            </div>
+
+        </div>
+    )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,46 +1,93 @@
 "use client"
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import { Card } from '../../components/Card';
+import data from '../data/campioni.json';
+import info from '../data/ItemsLol.json'
+import { random } from '../../components/random';
 
 export default function Home() {
+  const [number, setNumber] = useState(1);
+  const [champrRand, setChamprRand] = useState([]);
+  const [itRand, setItRand] = useState([]);
 
-  const [number,setNumber] = useState(0)
-
-
-  function inc(){
-    if(number<5 && number>=1)
-    setNumber((prev)=> prev+=1);
+  function inc() {
+    if (number < 5 && number >= 1) {
+      setNumber((prev) => prev + 1);
+    }
   }
 
-  function dec(){
-    if(number>1)
-    setNumber((prev)=> prev-= 1);
+  function dec() {
+    if (number > 1) {
+      setNumber((prev) => prev - 1);
+    }
+  }
+
+  function genera() {
+    const tempChamprRand = [];
+    const tempiTEMSrand = []
+
+    for (let i = 0; i < number; i++) {
+      tempChamprRand.push(data[random(0, 166)]);
+    }
+
+    for (let i = 0; i < number*6; i++) {
+      tempiTEMSrand.push(info[random(0, 105)]);
+    }
+    setChamprRand(tempChamprRand);
+    setItRand(tempiTEMSrand)
   }
 
   return (
     <>
-    <section className='flex flex-col items-center px-[4vw]'>
+      <section className='flex flex-col items-center px-[4vw]'>
+        <div className='py-[50px] flex items-center flex-col'>
+          <h1 className='text-center'>Random Build Lol Generator</h1>
+          <p className='md:w-[65ch] text-center'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+            tincidunt nec urna vel venenatis. Phasellus commodo efficitur
+            ullamcorper. Proin a interdum erat, ac varius mi. Aenean tempus
+            convallis augue iaculis volutpat. Cras efficitur, nibh quis
+            molestie fringilla.
+          </p>
+        </div>
 
-    <div className='py-[50px] flex items-center flex-col'>
-      <h1 className='text-center'>Random Build Lol Generator</h1>
-      <p className='md:w-[65ch] text-center'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tincidunt nec urna vel venenatis. Phasellus commodo efficitur ullamcorper. Proin a interdum erat, ac varius mi. Aenean tempus convallis augue iaculis volutpat. Cras efficitur, nibh quis molestie fringilla.</p>
-    </div>
+        <div className='flex gap-5 items-center pb-8'>
+          <button className='contanerAsd' onClick={inc}>
+            +
+          </button>
+          <div className='contanerNum'>{number}</div>
+          <button className='contanerAsd' onClick={dec}>
+            -
+          </button>
+        </div>
 
-    <div className='flex gap-5 items-center'>
+        <div>
+          <button
+            className='bottone md:px-28 w-full px-24'
+            onClick={genera}
+          >
+            Genera
+          </button>
+        </div>
 
-      <button className='contanerAsd' onClick={inc}>+</button>
+        <div className='flex md:flex-row flex-col md:items-start items-center justify-start w-full gap-3 pt-7'>
+          {champrRand.map((champion, index) => (
+            <Card
+              key={index}
+              nome={champion.nomeCampione}
+              imgCampione={champion.url_campione}
+              item1={itRand[index].img_url}
+              item2={itRand[index+1].img_url}
+              item3={itRand[index+2].img_url}
+              item4={itRand[index+3].img_url}
+              item5={itRand[index+4].img_url}
+              item6={itRand[index+5].img_url}
+            />
+          ))}
+        </div>
+      </section>
 
-      <div className='contanerNum'>{number}</div>
-
-      <button className='contanerAsd' onClick={dec}>-</button>
-    </div>
-    
-    
-
-
-    </section>
-
-    <img src='/dana.jpg' className='fixed object-cover object-center brightness-50 contrast-100 top-0 -z-10 h-full left-0 w-full'/>
-    
+      <img src='/dana.jpg' className='fixed object-cover object-center brightness-50 contrast-100 top-0 -z-10 h-full left-0 w-full' />
     </>
-  )
+  );
 }
